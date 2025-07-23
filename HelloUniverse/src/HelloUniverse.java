61adc79e7b1d2ff0b669666a0d966ca13c75c74f
pierrotdx <pierrotdx@gmail.com>
2025-07-23 22:09:00 +0200

    exercice 2

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -1,7 +1,13 @@
 public class HelloUniverse {
 
     public static void main(String... args){
-        System.out.println("Hello Universe!");
+        short nbPlanetes = 8;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
+        System.out.println(nbPlanetes);
+
+        nbPlanetes = 9;
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : ");
+        System.out.println(nbPlanetes);
     }
 
 }
