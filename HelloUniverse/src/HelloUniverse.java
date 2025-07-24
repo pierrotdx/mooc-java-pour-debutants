a91253efcda8737239c6e7b30a502fa95937f368
pierrotdx <pierrotdx@gmail.com>
2025-07-24 22:07:57 +0200

    exercice 8

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -1,17 +1,30 @@
 public class HelloUniverse {
 
     public static void main(String... args) {
-        short year = 2007;
+        short year = 2006;
+        short minYear = 1600;
+        short maxYear = 2020;
         short thresholdYear = 2006;
         short nbPlanetes;
 
-        if (year < thresholdYear) {
-            nbPlanetes = 9;
+        if (year < minYear || year > maxYear) {
+            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d", year);
         } else {
-            nbPlanetes = 8;
+             if (year < 1700) {
+                nbPlanetes = 7;
+             }
+             else if (year < 1800) {
+                nbPlanetes = 8;
+             }
+             else if (year <= 2006) {
+                nbPlanetes = 9;
+             }
+             else {
+                nbPlanetes = 8;
+             }
+             System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d", year, nbPlanetes);
         }
 
-        System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d", year, nbPlanetes);
     }
 
 }
