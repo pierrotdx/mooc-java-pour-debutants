6b5372b1cdc32a21901d72119fe09f8cb50cd65e
pierrotdx <pierrotdx@gmail.com>
2025-07-24 10:24:38 +0200

    bureau de change

---
+++ BureauDeChange/src/BureauDeChange.java
@@ -0,0 +1,9 @@
+public class BureauDeChange {
+    public static void main(String[] args) {
+        float rate = 1.22f;
+        short euroAmount = 1500;
+        float dollarAmount = rate * euroAmount;
+
+        System.out.println(dollarAmount);
+        }
+    }
