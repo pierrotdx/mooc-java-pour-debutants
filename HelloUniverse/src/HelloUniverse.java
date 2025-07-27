0633fd7b683bed18a967fab2fbe950a169679c49
pierrotdx <pierrotdx@gmail.com>
2025-07-27 21:48:52 +0200

    exercice 9

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -1,28 +1,19 @@
 public class HelloUniverse {
 
     public static void main(String... args) {
-        short year = 2006;
-        short minYear = 1600;
-        short maxYear = 2020;
-        short thresholdYear = 2006;
-        short nbPlanetes;
-
-        if (year < minYear || year > maxYear) {
-            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d", year);
-        } else {
-             if (year < 1700) {
-                nbPlanetes = 7;
-             }
-             else if (year < 1800) {
-                nbPlanetes = 8;
-             }
-             else if (year <= 2006) {
-                nbPlanetes = 9;
-             }
-             else {
-                nbPlanetes = 8;
-             }
-             System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d", year, nbPlanetes);
+        short nbPlanetes = 10;
+        switch (nbPlanetes) {
+            case 7:
+                System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                break;
+            case 8:
+                System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                break;
+            case 9:
+                System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                break;
+            default:
+                System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanetes);
         }
 
     }
