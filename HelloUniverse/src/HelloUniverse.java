1c5aca22ffb09dc8bd93b8a30b685c0107dd37f5
pierrotdx <pierrotdx@gmail.com>
2025-07-29 22:09:00 +0200

    exercice 30

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HelloUniverse {
     public static void main(String... args) {
         PlaneteTellurique mercure = new PlaneteTellurique("Mercure");
@@ -21,37 +23,69 @@ public class HelloUniverse {
         PlaneteGazeuse uranus = new PlaneteGazeuse("Uranus");
         uranus.diametre = 51118;
         uranus.atmosphere.tauxHydrogene = 83f;
-        uranus.atmosphere.tauxHelium= 15f;
+        uranus.atmosphere.tauxHelium = 15f;
         uranus.atmosphere.tauxMethane = 2.5f;
 
         PlaneteGazeuse neptune = new PlaneteGazeuse("Neptune");
         neptune.diametre = 49532;
 
-        int quantiteRefusee;
+        VaisseauDeGuerre chasseur = new VaisseauDeGuerre("CHASSEUR");
+        chasseur.nbPassagers = 4;
+        VaisseauDeGuerre fregate = new VaisseauDeGuerre("FREGATE");
+        fregate.nbPassagers = 9;
+        VaisseauDeGuerre croiseur = new VaisseauDeGuerre("CROISEUR");
+        croiseur.nbPassagers = 284;
+        VaisseauCivil cargo = new VaisseauCivil("CARGO");
+        cargo.nbPassagers = 938;
+        VaisseauCivil vaisseauMonde = new VaisseauCivil("VAISSEAU-MONDE");
+        vaisseauMonde.nbPassagers = 1;
 
-       VaisseauDeGuerre chasseur = new VaisseauDeGuerre("CHASSEUR");
-       terre.accueillirVaisseau(chasseur);
-       quantiteRefusee = chasseur.emporterCargaison(20);
-        System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
+        Scanner scanner = new Scanner(System.in);
 
-       VaisseauDeGuerre fregate = new VaisseauDeGuerre("FREGATE");
-       fregate.nbPassagers = 100;
-       terre.accueillirVaisseau(fregate);
-       quantiteRefusee = fregate.emporterCargaison(45);
-       System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
-       quantiteRefusee = fregate.emporterCargaison(12);
-       System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
+        System.out.println("Quel type de vaisseau voulez-vous manipuler ?");
+        String typeVaisseauChoisi = scanner.nextLine();
+        Vaisseau vaisseauChoisi;
+        switch (typeVaisseauChoisi) {
+            case "CHASSEUR":
+                vaisseauChoisi = chasseur;
+                break;
+            case "FREGATE":
+                vaisseauChoisi = fregate;
+                break;
+            case "CROISEUR":
+                vaisseauChoisi = croiseur;
+                break;
+            case "CARGO":
+                vaisseauChoisi = cargo;
+                break;
+            case "VAISSEAU-MONDE":
+            default:
+                vaisseauChoisi = chasseur;
+        }
 
-       VaisseauDeGuerre fregate2 = new VaisseauDeGuerre("FREGATE");
-       fregate2.nbPassagers = 14;
-       quantiteRefusee = fregate2.emporterCargaison(30);
-       System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
+        System.out.println("Sur quelle planète voulez-vous vous rendre ?");
+        String nomDePlaneteChoisie = scanner.nextLine();
+        PlaneteTellurique planeteChoisie;
+        switch (nomDePlaneteChoisie) {
+            case "Mercure":
+                planeteChoisie = mercure;
+                break;
+            case "Venus":
+                planeteChoisie = venus;
+                break;
+            case "Terre":
+                planeteChoisie = terre;
+                break;
+            case "Mars":
+            default:
+                planeteChoisie = mars;
+        }
+        planeteChoisie.accueillirVaisseau(vaisseauChoisi);
 
-       VaisseauCivil vaisseauMonde = new VaisseauCivil("VAISSEAU-MONDE");
-       terre.accueillirVaisseau(vaisseauMonde);
-        quantiteRefusee = vaisseauMonde.emporterCargaison(1560);
-        System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
-        quantiteRefusee = vaisseauMonde.emporterCargaison(600);
-        System.out.printf("La quantité refusée est de %d.\n", quantiteRefusee);
+        System.out.println("Quel tonnage de livraison souhaitez-vous embarquer ?");
+        int tonnageChoisi = scanner.nextInt();
+
+        int tonnageRejete = vaisseauChoisi.emporterCargaison(tonnageChoisi);
+        System.out.printf("Le tonnage rejeté est %d.\n", tonnageRejete);
     }
 }
